97293e4a3bd719c9ef03a35070e27fa287c211b9
ronald vilorio <soccer23415@gmail.com>
2018-07-06 00:33:01 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,9 +2,7 @@ class RepositoriesController < ApplicationController
 
   def search
     binding.pry
-    Github.get 'https://api.github.com/search/repositories' do |req|
-      binding.pry
-    end
+  
   end
 
   def github_search
