c1c60bdac478da85b6cf4506befcfa01b710d546
ronald vilorio <soccer23415@gmail.com>
2018-07-06 00:49:27 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,8 @@ class RepositoriesController < ApplicationController
 
   def github_search
     Faraday.get 'https://api.github.com/search/repositories' do |req|
-    
+      req.params['client_id'] = client_id
+      req.params['client_secret'] = client_secret
     end
   end
 end
